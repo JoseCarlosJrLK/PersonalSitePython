371df43a66fa43e78e8df2d63f01476db04cefc4
JoseCarlosJr <jose.sysadm@gmail.com>
2018-07-10 22:22:41 -0300

    Atualização do Django

--- MeuSite/Prg_Site/meu_site/settings.py
+++ MeuSite/Prg_Site/meu_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'meu_site.site_ini',
+    'meu_site.courses',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'meu_site', 'media')
