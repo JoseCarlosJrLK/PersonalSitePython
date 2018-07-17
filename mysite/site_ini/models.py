3485e5a012772320c367723c6153dfc4db7139a9
José Carlos Jr <jose.sysadm@gmail.com>
2018-07-17 09:45:31 -0300

    Montando a aba contato

--- mysite/site_ini/models.py
+++ mysite/site_ini/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class SendEmail(models.Model):
+
+    name = models.CharField('Nome', max_length=100)
+    email = models.EmailField('Email', max_length=100)
+    areas = models.TextField('Mensagem')
+
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    uploaded_at = models.DateTimeField('Atualizado em', auto_now=True)
